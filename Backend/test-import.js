1f42ee6157076e527c85d8cb3f0770f2765aab58
Aayush Tiwari <aayushtiwari1001@gmail.com>
2026-04-03 21:20:35 +0530

    test(backend): add import check js

---
+++ Backend/test-import.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var AuthService_1 = require("./src/services/AuthService");
+console.log('AuthService imported successfully:', typeof AuthService_1.default);
